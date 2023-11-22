4cdfcc1f74d54d8339423f6a5da667bbf60d07af
lucassilvajl <lucassilvajl25@gmail.com>
2023-11-22 15:30:05 -0400

    Atualização de dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,8 +5,8 @@ context('Funcionalidade Login', () => {
     it('Deve fazer login com sucesso', () => {
 
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('teste@teste.com', {log: false})
+        cy.get('#username').type('ls.teste@teste.com')
+        cy.get('#password').type('teste@teste.ls1234', {log: false})
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
         
@@ -15,8 +15,8 @@ context('Funcionalidade Login', () => {
     it('Deve aparecer um erro de e-mail não cadastrado', () => {
 
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        cy.get('#username').type('aluno_ebac@123.com')
-        cy.get('#password').type('teste@teste.com', {log: false})
+        cy.get('#username').type('ls_teste@teste.com')
+        cy.get('#password').type('teste@teste.ls1234', {log: false})
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
         
@@ -25,10 +25,10 @@ context('Funcionalidade Login', () => {
     it('Deve aparecer um erro de senha incorreta', () => {
 
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('teste.test.com', {log: false})
+        cy.get('#username').type('ls.teste@teste.com')
+        cy.get('#password').type('teste.teste.ls1234', {log: false})
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error').should('contain', 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('contain', 'Erro: a senha fornecida para o e-mail ls.teste@teste.com está incorreta. Perdeu a senha?')
 
         
     });
